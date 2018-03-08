0f44cfa5a2c74325fbcb06cbc276d48259fc83cf
andrewmic <37101387+andrewmic@users.noreply.github.com>
2018-03-08 09:59:51 +0200

    Review botes

--- src/swedbank/it/academy/service/LoanService.java
+++ src/swedbank/it/academy/service/LoanService.java
@@ -6,12 +6,14 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Date;
 
-/**
- * Created by p998feq on 2018.03.06.
- */
 public class LoanService implements LoanServiceInterface{
 
     private Loan[] loans;
+
+    //I don't know who told you that you need to save temp. values in LoanService, but he was wrong! :))
+    //LoanService should to have a state, in other words, you should not store temp. data in LoanService fields, instead,
+    // you should calculate data in methods and just return in.
+    //NOW, if I would call some methods multiple times, I would get different results :)
     private BigDecimal highRiskLoans = BigDecimal.ZERO;
     private BigDecimal averageLoanCost = BigDecimal.ZERO;
     private int count;
@@ -25,14 +27,28 @@ public class LoanService implements LoanServiceInterface{
     }
 
     //TASK1
+    //This should return subset (Loan array) of only high risk loans
     public Loan[] getHighRiskLoans() {
+        //Here is some code to get you on a right track
+/*        Loan[] highRiskLoans = new Loan[0];
+        int highRiskLoanIndex = 0;
         for (Loan loan : loans) {
+            if (loan.getRiskType() == LoanRiskType.HIGH_RISK) {
+                //TODO: Expand highRiskLoans by 1 element here
+                highRiskLoans[highRiskLoanIndex++] = loan;
+            }
+        }
+        return highRiskLoans;*/
+
+        for (Loan loan : loans) {
+            //You don't need to check if it is valid here
             if((loan.getRiskType() == LoanRiskType.HIGH_RISK) == loan.isValid())
                 Loan.highRiskCounter++;
         }
         return loans;
     }
 
+    //Calculation is correct, but I would call it second time, I would get different result (averageLoanCost is not being reset)
     public BigDecimal getAverageLoanCost(){
         for (Loan loan : loans)
             averageLoanCost = averageLoanCost.add(loan.getPrice().multiply(BigDecimal.ONE.add(loan.getInterestRate().divide(BigDecimal.valueOf(100)))));
@@ -40,10 +56,12 @@ public class LoanService implements LoanServiceInterface{
         return averageLoanCost.divide(BigDecimal.valueOf(loans.length)).setScale(1, RoundingMode.CEILING);
     }
 
+    //Calculation is correct
     public BigDecimal getAverageLoanCost(LoanRiskType loanRiskType){
         count = 0;
         averageLoanCost = BigDecimal.ZERO;
         for (Loan loan : loans){
+            //You don't need to check if it is valid here
             if((loanRiskType == loan.getRiskType()) == loan.isValid()) {
                 count++;
                 averageLoanCost = averageLoanCost.add(loan.getPrice().add(loan.getPrice().multiply(loan.getInterestRate().divide(BigDecimal.valueOf(100)))));
@@ -53,6 +71,8 @@ public class LoanService implements LoanServiceInterface{
     }
 
     public BigDecimal getAverageCostOfHighRiskLoans(){
+        //You can re-use method above
+        //return getAverageLoanCost(LoanRiskType.HIGH_RISK);
         count = 0;
         for (Loan loan : loans){
             if((LoanRiskType.HIGH_RISK == loan.getRiskType()) == loan.isValid()) {
@@ -64,6 +84,19 @@ public class LoanService implements LoanServiceInterface{
     }
 
     public BigDecimal getMaximumPriceOfNonExpiredLoans(){
+        //Wayyyy too complicated
+        //See me approach
+/*
+        BigDecimal maximumPrice = BigDecimal.ZERO; //Let's say that min. possible price is 0
+        for (Loan loan : loans) {
+            BigDecimal price = loan.getPrice();
+            if (loan.isValid() && //Loan.isValid() SHOULD do this check already (DateUtil.addYears(loan.getCreationDate(), loan.getTermInYears()).after(new Date()))
+                price.compareTo(maximumPrice) > 0) {
+                maximumPrice = price;
+            }
+        }
+        return maximumPrice;
+        */
         for (Loan loan : loans){
             if((DateUtil.addYears(loan.getCreationDate(), loan.getTermInYears()).after(new Date())) == loan.isValid()){
                 temp.equals(loan.getPrice());
@@ -81,8 +114,11 @@ public class LoanService implements LoanServiceInterface{
         return temp;
     }
     //TASK2
+
+    //This should return only normal risk "VehicleLoan"s. See what I've wrote about "getHighRiskLoans()"
     public Loan[] getNormalRiskVehicleLoans(){
         for (Loan loan: loans) {
+            //You don't need to check if it is valid here
             if ((loan instanceof VehicleLoan && loan.getRiskType() == LoanRiskType.NORMAL_RISK) == loan.isValid()) {
                 VehicleLoan.normalRiskVehicleLoansCounter++;
             }
@@ -90,19 +126,22 @@ public class LoanService implements LoanServiceInterface{
         return loans;
     }
 
+    //Again, wayyy too complicated approach. See my comment on getMaximumPriceOfNonExpiredLoans()
     public int getMaximumAgeOfLowRiskLoanedVehicles() {
 
         for (Loan loan: loans) {
+            //You don't need to check if it is valid here
             if ((loan instanceof VehicleLoan && loan.getRiskType() == LoanRiskType.LOW_RISK) == loan.isValid()) {
-
+                //Age should be calculated in VehicleLoan.getAge()
                 tempMaximumAgeOfLowRiskLoanedVehicles = ((int)DateUtil.differenceInDays(loan.getCreationDate(),((VehicleLoan) loan).getManufactured())/365);
 
                 break;
             }
         }
         for (Loan loan: loans){
-
+            //You don't need to check if it is valid here
             if((loan instanceof VehicleLoan && loan.getRiskType() == LoanRiskType.LOW_RISK) == loan.isValid()) {
+                //Age should be calculated in VehicleLoan.getAge()
                 if (tempMaximumAgeOfLowRiskLoanedVehicles < DateUtil.differenceInDays(loan.getCreationDate(),((VehicleLoan) loan).getManufactured())/365)
                     tempMaximumAgeOfLowRiskLoanedVehicles = (int)DateUtil.differenceInDays(loan.getCreationDate(),((VehicleLoan) loan).getManufactured())/365;
             }
@@ -112,14 +151,18 @@ public class LoanService implements LoanServiceInterface{
         return maximumAgeOfLowRiskLoanedVehicles;
     }
 
+    //This should return only "RealEstateLoan"s with purpose == RealEstatePurpose.PERSONAL. See what I've wrote about "getHighRiskLoans()"
     public Loan[] getPersonalRealEstateLoans(){
         for (Loan loan: loans){
+            //You don't need to check if it is valid here
             if((loan instanceof RealEstateLoan && (((RealEstateLoan) loan).getPurpose() == String.valueOf(RealEstatePurpose.PERSONAL))) == loan.isValid()){
                 RealEstateLoan.personalRealEstateLoans++;
             }
         }
         return loans;
     }
+
+    //This should return only "VehicleLoan"s with riskType == LoanRiskType.HIGH_RISK && !loan.isValid() && max(loan.getTermInYears()). See what I've wrote about "getHighRiskLoans()"
     public  Loan[] getExpiredHighRiskVehicleLoansOfHighestDuration(){
 
         for (Loan loan: loans){
@@ -130,6 +173,7 @@ public class LoanService implements LoanServiceInterface{
         }
 
         for (Loan loan: loans){
+            //Loan.isValid() SHOULD do this check already
             if((loan instanceof VehicleLoan && loan.getRiskType() == LoanRiskType.HIGH_RISK && DateUtil.addYears(loan.getCreationDate(), loan.getTermInYears()).before(new Date())) == loan.isValid()){
                 VehicleLoan.expiredHighRiskVehicleLoansCounter++;
 
