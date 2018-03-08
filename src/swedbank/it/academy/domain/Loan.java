161c7a65a5dfffc7783ba302d2a4ef3d42dca609
andrewmic <37101387+andrewmic@users.noreply.github.com>
2018-03-08 09:58:07 +0200

    Review notes

--- src/swedbank/it/academy/domain/Loan.java
+++ src/swedbank/it/academy/domain/Loan.java
@@ -2,17 +2,19 @@ package swedbank.it.academy.domain;
 import java.math.BigDecimal;
 import java.util.Date;
 
-/**
- * Created by p998feq on 2018.03.06.
- */
+
 public class Loan {
 
     private Date creationDate;
     private int termInYears;
     private String name;
+    //This should be private
+    //VehicleLoan should setInterestRate(...) instead of directly accessing it
     protected BigDecimal interestRate;
     private BigDecimal price;
     private LoanRiskType riskType;
+    //A Loan should not know anything about other Loans.
+    //LoanService.getHighRiskLoans() should count this value internally every time it is called
     public static int highRiskCounter = 0;
 
     public void setCreationDate(Date creationDate) {
@@ -58,8 +60,12 @@ public class Loan {
         return riskType;
     }
 
+    //I believe this should be calculated value and to a constant :)
     public boolean isValid(){
         return true;
     }
 
+    //Missing methods:
+    //calculateInterest()
+    //getTotalLoanCost()
 }
